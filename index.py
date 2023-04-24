2c439a7ebd3fc037360d5e5f1440fe133e4e2b34
pkun2 <pkun2@naver.com>
2023-04-24 19:30:46 +0900

    mongodb 연결

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 import requests
+from db import dbConect, insertDB
 from bs4 import BeautifulSoup
 
 url = "https://www.musinsa.com/categories/item/002" #크롤링할 사이트
@@ -9,11 +10,13 @@ response.raise_for_status() #http 오류 발생 검증 코드
 
 soup = BeautifulSoup(response.text, "lxml") #lxml형태로 불러옴
 
+dbConect()
 
 cloth_list = soup.select("#searchList > li") #리스트 데이터 
 
 for cloth in cloth_list:
     name = cloth.select_one("#searchList > li >  div.li_inner > div.article_info > p.list_info > a")["title"] #무신사 옷의 제목 가져오기
+    insertDB(name);
     print(name)
 
 #searchList > li:nth-child(1) > div.li_inner > div.article_info > p.list_info > a
